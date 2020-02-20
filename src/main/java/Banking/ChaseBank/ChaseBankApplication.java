493fa2ac364876042f450e2d23a524cd56639060
bbkssh <byksh_shr@live.com>
2020-02-20 00:59:55 -0600

    Initial commit for sharing main files

---
+++ src/main/java/Banking/ChaseBank/ChaseBankApplication.java
@@ -0,0 +1,14 @@
+package Banking.ChaseBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaseBankApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChaseBankApplication.class, args);
+	}
+
+}
