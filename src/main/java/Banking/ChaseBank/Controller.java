2826332901626be4ae5882d119608bbaaefc25c4
bbkssh <byksh_shr@live.com>
2020-02-21 00:53:53 -0600

    small change for push

--- src/main/java/Banking/ChaseBank/Controller.java
+++ src/main/java/Banking/ChaseBank/Controller.java
@@ -1,4 +1,49 @@
 package Banking.ChaseBank;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/Chasebank")
+@RestController
 public class Controller {
+
+    @Autowired
+    private Service service;
+
+    @PostMapping("/deposit1") //first deposit
+    public int adda1(@RequestBody DTO depositItems){
+        service.a1.add(depositItems.getAmount());
+        return service.a1.stream().mapToInt(a -> a).sum();
+    }
+
+    @PostMapping("/deposit2") //second deposit
+    public int adda2(@RequestBody DTO depositItems){
+        service.a2.add(depositItems.getAmount());
+        return service.a2.stream().mapToInt(a -> a).sum();
+    }
+
+    @GetMapping("/deposit/{depositNumber1}/{depositNumber2}")
+    public int deposit(@PathVariable("depositNumber1") int depositNumber1,
+                       @PathVariable("depositNumber2") int depositNumber2){
+        return service.aList.get(depositNumber1 - 1).get(depositNumber2 - 1);
+    }
+
+    @GetMapping("/history")
+    public List history(){
+        service.aList.add(0,service.a1);
+        service.aList.add(1,service.a2);
+        return service.aList;
+    }
+
+    @GetMapping("/history1")
+    public List history1(){
+        return service.a1;
+    }
+
+    @GetMapping("/history2")
+    public List history2(){
+        return service.a2;
+    }
 }
--- src/main/java/Banking/ChaseBank/Service.java
+++ src/main/java/Banking/ChaseBank/Service.java
@@ -2,7 +2,14 @@ package Banking.ChaseBank;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public class Service {
 
+    //aList is 2D arraylist declaration
+    ArrayList<ArrayList<Integer>> aList = new ArrayList<ArrayList<Integer>>();
+
+    ArrayList<Integer> a1 = new ArrayList<Integer>(); //arraylist for 1st deposit
+    ArrayList<Integer> a2 = new ArrayList<Integer>(); //arraylist for 2nd deposit
 }
